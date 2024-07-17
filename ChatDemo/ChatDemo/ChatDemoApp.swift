6e1e437ae26c2737c53ffe66ba871834e33e2080
KitsuneNoctus <theceintiant@gmail.com>
2024-07-17 13:00:11 -0700

    Chat demo app

---
+++ ChatDemo/ChatDemo/ChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoApp.swift
+//  ChatDemo
+//
+//  Created by Henry Calderon on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
